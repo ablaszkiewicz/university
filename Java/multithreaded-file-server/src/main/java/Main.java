69a5f21ed29a1155c7d6362c5ad209b9ceab18d3
Aleksander Błaszkiewicz <54682207+ablaszkiewicz@users.noreply.github.com>
2021-03-26 10:50:45 +0100

    Update threading so that all threads stop properly

--- Java/multithreaded-file-server/src/main/java/Main.java
+++ Java/multithreaded-file-server/src/main/java/Main.java
@@ -1,18 +1,28 @@
 import java.io.File;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws Exception {
         int port = 6666;
-        Thread serverThread = new Thread(new Server(port));
+        Server server = new Server(port);
+        Thread serverThread = new Thread(server);
         serverThread.start();
-
+        ArrayList<Thread> clientThreads = new ArrayList<Thread>();
         System.in.read();
 
         for(int i=0; i<1; i++) {
             Client client = new Client(port, "send" + i + ".txt");
             Thread clientThread = new Thread(client);
+            clientThreads.add(clientThread);
             clientThread.start();
         }
+
+        for(Thread thread : clientThreads) {
+            thread.join();
+        }
+        System.out.println("Closed all client threads");
+        server.close();
+        serverThread.join();
     }
 }
--- Java/multithreaded-file-server/src/main/java/Server.java
+++ Java/multithreaded-file-server/src/main/java/Server.java
@@ -1,31 +1,62 @@
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.net.SocketException;
+import java.util.ArrayList;
 
 public class Server implements Runnable {
     private int port;
     private int id;
+    private ArrayList<Thread> threads = new ArrayList<Thread>();
+    private ServerSocket serverSocket;
+
     public Server(int port) {
         System.out.println("Created server on port: " + port);
         this.port = port;
     }
+
     @Override
     public void run() {
-        try (ServerSocket server = new ServerSocket(port)) {
-            System.out.println("Started server on port: " + port);
-            while(!Thread.interrupted()) {
-                try {
-                    Socket socket = server.accept();
-                    System.out.println("Someone connected");
-                    Thread clientThread = new Thread(new ClientHandler(socket, "receive" + id + ".txt"));
-                    id++;
-                    clientThread.start();
-                } catch (IOException ex) {
-                    System.err.println(ex);
+
+            try {
+                serverSocket = new ServerSocket(port);
+                System.out.println("Started server on port: " + port);
+                while(!Thread.interrupted()) {
+                    try {
+                        Socket socket = serverSocket.accept();
+                        System.out.println("Someone connected");
+                        Thread clientHandlerThread = new Thread(new ClientHandler(socket, "receive" + id + ".txt"));
+                        id++;
+                        clientHandlerThread.start();
+                        threads.add(clientHandlerThread);
+                    } catch(SocketException e) {
+                        System.out.println("Closed server thread");
+                        Thread.currentThread().interrupt();
+                        break;
+                    }
+                    catch (IOException ex) {
+                        System.err.println(ex);
+                    }
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+    }
+
+    public void close() {
+        try {
+            for(Thread thread : threads) {
+                if(thread.isAlive()) {
+                    thread.join();
                 }
             }
-        } catch (IOException ex) {
-            System.err.println(ex);
+            System.out.println("Closed all ClientHandler threads");
+            serverSocket.close();
+        } catch(IOException e) {
+            e.printStackTrace();
+        }
+        catch(InterruptedException e) {
+            e.printStackTrace();
         }
     }
 }
