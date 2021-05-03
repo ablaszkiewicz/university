3116ed82723f11e4d6bed7b8a8659684b5123480
Aleksander Błaszkiewicz <54682207+ablaszkiewicz@users.noreply.github.com>
2021-05-03 18:43:54 +0200

    Implement task A, B and C

--- Python/linear-equations/main.py
+++ Python/linear-equations/main.py
@@ -20,7 +20,61 @@ A[i == j] = a1
 A[i == j-1] = a2
 A[i == j-2] = a3
 
-b = np.fromfunction(lambda i, j: np.sin(j*(f+1)), (1, N));
+b = np.fromfunction(lambda i, j: np.sin(j*(f+1)), (1, N))
 
-# -------- TASK B1 --------
+# -------- TASK B GAUSS --------
 
+def gauss(A, b, min_norm):
+    iter = 0
+
+    x = np.zeros((b.size, 1))
+    L = np.tril(A)
+    U = A - L
+
+    current_norm = np.inf
+    while current_norm > min_norm:
+        x = np.linalg.inv(L)@(b - U@x)
+        current_norm = np.linalg.norm(np.matmul(A, x) - b)
+        iter += 1
+
+        assert not np.isnan(current_norm), "Norm is nan"
+        assert not np.isinf(current_norm), "Norm is inf"
+    return x, iter
+
+
+result, iter = gauss(A, b, 10 ** -6)
+print("Iteracje gauss:", iter)
+
+# -------- TASK B JACOBI --------
+
+def jacobi(A, b, min_norm):
+    iter = 0
+
+    x = np.zeros((b.size, 1))
+    L = np.tril(A, -1)
+    U = np.triu(A, 1)
+    D = np.diag(A)
+
+    fst = -(L + U) / D
+    snd = b / D
+
+    current_norm = np.inf
+    while current_norm > min_norm:
+        x = fst@x + snd
+        current_norm = np.linalg.norm(np.matmul(A, x) - b)
+        iter += 1
+
+        assert not np.isnan(current_norm), "Norm is nan"
+        assert not np.isinf(current_norm), "Norm is inf"
+    return x, iter
+
+
+result, iter = jacobi(A, b, 10 ** -6)
+print("Iteracje jacobi:", iter)
+
+# -------- TASK C --------
+
+# Wywołałem powyższy kod dla wartości a1=3 i dla obydwu metod wyrzuca
+# assert np.isinf(current_norm), więc metody nie zbiegają się
+
+# -------- TASK D --------
