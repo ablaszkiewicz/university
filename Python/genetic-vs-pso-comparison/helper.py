8ac46a0346bd024d53c44b7f1ab24db8c68c5424
Aleksander Błaszkiewicz <54682207+ablaszkiewicz@users.noreply.github.com>
2021-05-08 14:15:48 +0200

    Add individual particle best position calculation

--- Python/genetic-vs-pso-comparison/helper.py
+++ Python/genetic-vs-pso-comparison/helper.py
@@ -7,6 +7,7 @@ class Point:
         self.y = y
         self.x_velocity = 0
         self.y_velocity = 0
+        self.best_fitness = math.inf
         self.best_x = 0
         self.best_y = 0
 
@@ -16,8 +17,14 @@ class Point:
     def distance(self, other):
         delta_x = math.fabs(other.x - self.x)
         delta_y = math.fabs(other.y - self.y)
-        return math.abs(delta_x ** 2 + delta_y ** 2)
+        return math.fabs(delta_x ** 2 + delta_y ** 2)
 
     def move(self):
         self.x += self.x_velocity
         self.y += self.y_velocity
+
+    def calculate_self_best_position(self):
+        if(self.fitness() < self.best_fitness):
+            self.best_x = self.x
+            self.best_y = self.y
+
--- Python/genetic-vs-pso-comparison/pso.py
+++ Python/genetic-vs-pso-comparison/pso.py
@@ -3,31 +3,35 @@ import random
 import helper
 
 # Configuration
-iterations = 100
+iterations = 30
 
 # Population
 population_size = 10
 
 # Function
-function_min = -5
-function_max = 5
+function_min = 100
+function_max = -100
 
 # PSO variables
 
 w = 0.7 # interia
-c1 = 1.5 # cognitive (self)
-c2 = 1.5 # social (swarm)
-swarm_best_position = helper.Point(0, 0)
+cognitive_coefficient = 1.5 # cognitive (self)
+social_coefficient = 1.5 # social (swarm)
+swarm_best_position = helper.Point(function_min, function_max)
+
+# Plot variables
+
+distances_to_best_point = []
 
 
 def random_in_range():
     return float("%.1f" % random.uniform(function_min, function_max))
 
 
-def show_plot():
+def iter_plot():
     x_values = [point.x for point in points]
     y_values = [point.y for point in points]
-    plt.scatter(x_values, y_values)
+    axs[0].scatter(x_values, y_values)
     plt.pause(0.1)
 
 
@@ -41,12 +45,16 @@ def calculate_new_velocities(points):
         learning_part_x = w * point.x_velocity
         learning_part_y = w * point.y_velocity
 
-        # social part
-        social_part_x = random.random() * c2 * (swarm_best_position.x - point.x)
-        social_part_y = random.random() * c2 * (swarm_best_position.y - point.y)
+        # cognitive part (self learning)
+        cognitive_part_x = cognitive_coefficient * random.random() * (point.best_x - point.x)
+        cognitive_part_y = cognitive_coefficient * random.random() * (point.best_y - point.y)
 
-        point.x_velocity = learning_part_x + social_part_x
-        point.y_velocity = learning_part_y + social_part_y
+        # social part
+        social_part_x = random.random() * social_coefficient * (swarm_best_position.x - point.x)
+        social_part_y = random.random() * social_coefficient * (swarm_best_position.y - point.y)
+
+        point.x_velocity = learning_part_x + cognitive_part_x + social_part_x
+        point.y_velocity = learning_part_y + cognitive_part_y + social_part_y
 
 
 def move_points(points):
@@ -56,15 +64,31 @@ def move_points(points):
 
 def calculate_best_position(points):
     for point in points:
-        if point.fitness() > swarm_best_position.fitness():
+        if point.fitness() < swarm_best_position.fitness():
             swarm_best_position.x = point.x
             swarm_best_position.y = point.y
 
 
+def calculate_self_best_positions(points):
+    for point in points:
+        point.calculate_self_best_position()
+
+
 points = random_population()
+fig, axs = plt.subplots(1, 2)
+axs[0].set_title("Punkty")
 
 for _ in range(iterations):
-    show_plot()
+    iter_plot()
+    calculate_best_position(points)
     calculate_new_velocities(points)
     move_points(points)
+    distances_to_best_point.append(swarm_best_position.distance(helper.Point(0, 0)))
+    print(swarm_best_position.x, swarm_best_position.y)
+
+
+axs[1].plot(list(range(iterations)), distances_to_best_point)
+axs[1].set_title("Najlepszy dystans")
+plt.show()
+
 
